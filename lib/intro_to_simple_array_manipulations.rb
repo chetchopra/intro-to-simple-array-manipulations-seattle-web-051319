4d8460dc233c7716baf2bccab17697ce21e3330f
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 17:32:42 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push(arr, st)
+  return arr.push(st)
+end 
+
+def using_unshift(arr, st)
+  return arr.unshift(st)
+end
+
+def using_pop(arr)
+  return arr.pop()
+end
+
+def pop_with_args(arr)
+  return arr.pop(2)
+end
+
+def using_shift(arr)
+  return arr.shift()
+end
+
+def shift_with_args(arr)
+  return arr.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  
